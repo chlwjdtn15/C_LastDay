61b30ce1652adaab3b1de5c359e71e23a5e30097
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-07-06 02:48:25 +0000

    malloc (memory allocation) 이용해서 풀기

--- main.c
+++ main.c
@@ -16,16 +16,75 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define true 1
-#define SIZE 3 // 수정 불가
+// #define SIZE 3 // 수정 불가
 
 int main(void) {
   
-  int ages[SIZE]; // 수정 불가
+//   int ages[SIZE]; // 수정 불가
+
+
+// int count;
+
+// while(true) {
+//   printf("사람의 숫자를 입력해 주세요 : ");
+//   scanf("%d", &count);
+
+//   if(count > 0 && count <= 3) {
+//     break;
+
+
+//   } else {
+//     printf("3이하로 입력해주세요.\n");
+//   }
+// }
+
+// for(int i = 0; i < count; i++) {
+
+//   printf("%d번째 사람의 나이를 입력해 주세요 : ", i + 1);
+
+//     scanf("%d", &ages[i]);
+
+
+    
+
+// }
+
+// for(int i = 0; i < count; i++) {
+//   printf("%d번째 사람의 나이 : %d\n", i + 1, ages[i]);
+// }
+
+
+//========================================================================//
+
+// 문제 : 몇명의 사람의 나이를 받을지 입력받고 입력받은 수 만큼 나이를 입력받은 후 저장해주세요.
+// 조건 : malloc을 사용해서 낭비없는 메모리 운용을 보여주세요.
+// 출력예시
+/*
+사람의 숫자를 입력해주세요. : 5[엔터]
+1번째 사람의 나이를 입력해주세요 : 33[엔터]
+2번째 사람의 나이를 입력해주세요 : 50[엔터]
+3번째 사람의 나이를 입력해주세요 : 42[엔터]
+4번째 사람의 나이를 입력해주세요 : 19[엔터]
+5번째 사람의 나이를 입력해주세요 : 27[엔터]
+1번째 사람의 나이 : 33
+2번째 사람의 나이 : 50
+3번째 사람의 나이 : 42
+4번째 사람의 나이 : 19
+5번째 사람의 나이 : 27
+*/
+
+int* ages = (int*)malloc(sizeof(int));
+
 
 
 int count;
+
+if (ages == NULL) {
+  printf("malloc error");
+}
 
 while(true) {
   printf("사람의 숫자를 입력해 주세요 : ");
@@ -54,6 +113,10 @@ for(int i = 0; i < count; i++) {
 for(int i = 0; i < count; i++) {
   printf("%d번째 사람의 나이 : %d\n", i + 1, ages[i]);
 }
+
+
+  free(ages);
+ 
 
   return 0;
 }
